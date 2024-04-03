df51ac7a0df3110524f9270fb33e370cfd91579c
Aashif <aashif3225@gmail.com>
2024-04-03 13:11:24 +0530

    add more content

--- Reat&spread.js
+++ Reat&spread.js
@@ -21,42 +21,52 @@ function print(a,b,c,...input){
     console.log(input);
 }
 print(1,2,3,4,5,6,7,8,9,10);*/
-
+/*
 //spread Operator
 
-//   let array=[10,20,30,40,50,60,70];
-// function spread(a,b,c,d){
-//     // console.log(a);
-//     // console.log(b);
-//     // console.log(c);
-//     console.log(...array);
+  let array=[10,20,30,40,50,60,70];
+function spread(a,b,c,d){
+    // console.log(a);
+    // console.log(b);
+    // console.log(c);
+    console.log(...array);
 
-// }
+}*/
+
 /*
 //copy of array using spread
 let array=[10,20,30,40]
 let array1=[...array];
-// console.log(array1);//10,20,30,40
+//console.log(array1);//10,20,30,40
 array1[4]=50;
-console.log(array);*/// it will not modify original array
+console.log(array);*/// it will not modify original array*/
 
+/*
 //create copy of object
-// let object1={
-//     name:"Aashif",
-//     age:22,
-//     city:"Noida",
+let object1={
+    name:"Aashif",
+    age:22,
+    city:"Noida",
 
-// }
-// let object2={...object1}
-// console.log(object2);
-// console.log(object1);
+}
+let object2={...object1}
+console.log(object2);
+console.log(object1);*/
 
-//  spread operator
-// let array=[10,20,30,40,50,60]
-// function spread(a,b,c){
-//     console.log(...array)
-//     console.log(a);
-//     console.log(b);
-//     console.log(c);
-// }
-// spread(...array);
+ /*Spread operator
+let array=[10,20,30,40,50,60]
+function spread(a,b,c){
+    console.log(...array)
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+spread(...array);*/
+
+// Rest Parameter can be used in array destructuring....
+let array=[1,20,30,40,50,60,70,80,90,100];
+let[a,b,c,...rest]=array;//array destructuring using Rest Parameter..
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(...rest);
